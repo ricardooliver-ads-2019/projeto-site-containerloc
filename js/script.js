ac5f643047baa7c40170c6f00398a8644c5cbe82
ricardooliver-ads-2019 <ricardo.oliver.ads.2019@gmail.com>
2021-01-22 20:57:33 -0400

    buttons_carousel

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
 /* --- ---- section carousel_containers ---- --- */
 document.querySelector(".items").addEventListener("wheel", event =>{
     if(event.deltaY > 0){
-        event.target.scrollBy(300, 0)
+        event.target.scrollBy(400, 0)
 
     }else{
         event.target.scrollBy(-300, 0)
     }
     })
+
+
+/*
+* Nav buttons proximo
+*/
+document.querySelector('button#prox').addEventListener('click', function(event){
+    document.querySelector('.items').scrollBy(300, 0);
+  });
+  
+  document.querySelector('button#ant').addEventListener('click', function(event){
+    document.querySelector('.items').scrollBy(-300, 0);
+  });
+  /*
+  * Nav buttons end
+  */
